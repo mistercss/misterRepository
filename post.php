e0b132bedf47f12268a2ba6ba2dbb7e141ce6226
mistercss <denisselivankin@yandex.ru>
2013-09-17 17:57:17 +0300

    1

---
+++ post.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+$result00 = mysql_query("SELECT COUNT(*) FROM data WHERE secret=0");
+$temp = mysql_fetch_array($result00);
+$posts = $temp[0];
+
+$result = mysql_query("SELECT * FROM `data` WHERE `secret`=0  ORDER BY `id`");
+
+
+if (mysql_num_rows($result) > 0)
+
+{
+$myrow = mysql_fetch_array($result);
+
+do
+{
+
+
+
+echo ="
+		<table>	
+		<tr>
+         <td ><strong class='titl'><a class='News' href='fullstory?id=".$myrow["id"]."'>".$myrow["title"]."</a> / ".$myrow["date"]."</strong></td>
+         </tr>
+
+         <tr>
+         <td>".$myrow["description"]."</td>
+		 
+		 <td>".$myrow["view"]."</td>
+		 <td>".$myrow["date"]."</td>
+		</table>
+
+
+        
+
+
+
+}
+while ($myrow = mysql_fetch_array($result));
+
+
+
+
+
+?>
